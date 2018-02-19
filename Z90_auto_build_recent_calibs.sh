fc63b93a8a3858d35414ab5dc14d15601fe25213
Robert Siverd <robert.siverd@gmail.com>
2018-02-19 02:07:02 -0800

    Progress on this script has resumed. Can now get output of choose-dayobs inside script. Also load date/time functions from func/01_time_and_date.sh.

--- Z90_auto_build_recent_calibs.sh
+++ Z90_auto_build_recent_calibs.sh
@@ -7,14 +7,14 @@
 #
 # Rob Siverd
 # Created:      2017-08-14
-# Last updated: 2017-08-14
+# Last updated: 2019-02-18
 #--------------------------------------------------------------------------
 #**************************************************************************
 #--------------------------------------------------------------------------
 
 ## Default options:
 debug=0 ; clobber=0 ; force=0 ; timer=0 ; vlevel=0
-script_version="0.10"
+script_version="0.15"
 this_prog="${0##*/}"
 #shopt -s nullglob
 # Propagate errors through pipelines: set -o pipefail
@@ -50,7 +50,7 @@ trap "$jnk_cleanup" EXIT
 
 ## Required programs:
 declare -a need_exec
-need_exec+=( awk cat FuncDef sed tr )
+need_exec+=( awk cat choose-dayobs FuncDef sed tr )
 #need_exec+=( shuf shuffle sort ) # for randomization
 for need in ${need_exec[*]}; do
    if ! ( /usr/bin/which $need >& /dev/null ); then
@@ -81,9 +81,57 @@ fi
 camid="$1"
 ndays="$2"
 
+##--------------------------------------------------------------------------##
+## Date/time manipulation:
+date_funcs="func/01_time_and_date.sh"
+[ -f $date_funcs ] || ErrorAbort "Can't find file: $date_funcs"
+vcmde "source $date_funcs"
+
 ##**************************************************************************##
 ##==========================================================================##
 ##--------------------------------------------------------------------------##
+
+## Load site<-->camera mapplings:
+site_cams="aux/site_camera_mappings.sh"
+[ -f $site_cams ] || ErrorAbort "Can't find file: $site_cams" 
+eval "$(cat $site_cams)"
+
+#echo "camera_from_site:"
+#echo "${camera_from_site[@]}"
+
+lsite=${site_from_camera[$camid]}
+echo "Selected camera:  $camid"
+echo "LCO network site: $lsite"
+
+### Run choose-dayobs and process dict() output:
+#choose-dayobs -q -s $lsite | tr -d \{ | tr -d \} | tr ',' '\n' \
+#   | tr -d ' ' | tr -d \' > $foo
+##cmde "cat $foo"
+#proc_fdate=$(grep process_fdate $foo | cut -d: -f2)
+##echo "rstring: $rstring"
+#echo $rstring | tr ',' '\n' | tr -d ' ' | tr -d "'"
+
+## Run choose-dayobs and process dict() output:
+#arrname="dchoice"
+#eval "$(choose-dayobs -q -s lsc \
+#   | sed -e "s/^{'/'/" -e "s/'}$/'/" -e "s/', '/'\n'/g" \
+#   | awk -v arr="$arrname" -F\' '
+#   BEGIN { printf "declare -A %s\n", arr }
+#   { 
+#      printf "%s+=( [\"%s\"]=\"%s\" )\n", arr, $2,$4
+#   }')"
+#echo "dchoice:"
+#for i in "${!dchoice[@]}"; do
+#   printf "%20s:%20s\n" $i ${dchoice[$i]}
+#done
+
+## Ingest results from choose-dayobs:
+arrname="dchoice"
+eval "$(choose-dayobs -q -s $lsite --bash dchoice)"
+
+proc_fdate="${dchoice[process_fdate]}"
+echo "proc_fdate: $proc_fdate"
+
 
 ## List of recent nights:
 declare -a nite_list
@@ -105,6 +153,10 @@ exit 0
 ######################################################################
 # CHANGELOG (Z90_auto_build_recent_calibs.sh):
 #---------------------------------------------------------------------
+#
+#  2018-02-18:
+#     -- Increased script_version to 0.15.
+#     -- Resumed implementation using new choose-dayobs Python script.
 #
 #  2017-08-14:
 #     -- Increased script_version to 0.10.

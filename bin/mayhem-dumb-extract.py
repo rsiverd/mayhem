ee221a49315c634757bf94a61b2eeb523a9bcec8
Rob Siverd <robert.siverd@gmail.com>
2018-12-24 04:12:42 -0600

    other minor updates for TraceData object

--- bin/mayhem-dumb-extract.py
+++ bin/mayhem-dumb-extract.py
@@ -276,7 +276,7 @@ if (spec_data.shape != lamp_data.shape):
 ## Generate pixel masks from trace parameters:
 sys.stderr.write("Calculating pixels from trace parameters ... ")
 trace_pixel_pos = nrex.mask_from_traces(spec_data.shape, 
-                                trdata.get_params_list())
+                                trdata.get_trace_list())
 sys.stderr.write("done.\n")
 
 ## Extract data at specified positions:

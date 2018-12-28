9ed9bbaf5fe23f89990efabf8e21c38f8709819a
Rob Siverd <robert.siverd@gmail.com>
2018-12-28 15:16:07 -0600

    Initial commit of new test code. Mixed results so far.

---
+++ devel/channel_correl_test.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 ts=4 sts=4 sw=4 et tw=80 :
+#
+# Quick test of viability of using cross-correlation for order identification.
+# In essence, this script is meant to determine how similar the spectrum
+# images are for the various telescopes.
+#
+# Rob Siverd
+# Created:       2018-12-28
+# Last modified: 2018-12-28
+#--------------------------------------------------------------------------
+#**************************************************************************
+#--------------------------------------------------------------------------
+
+## Current version:
+__version__ = "0.0.1"
+
+## Python version-agnostic module reloading:
+try:
+    reload                              # Python 2.7
+except NameError:
+    try:
+        from importlib import reload    # Python 3.4+
+    except ImportError:
+        from imp import reload          # Python 3.0 - 3.3
+
+## Modules:
+#import argparse
+import signal
+import glob
+import os
+import sys
+import time
+import numpy as np
+#from numpy.lib.recfunctions import append_fields
+#import datetime as dt
+#from dateutil import parser as dtp
+#import scipy.linalg as sla
+#import scipy.signal as ssig
+#import scipy.ndimage as ndi
+#import scipy.optimize as opti
+#import scipy.interpolate as stp
+#import scipy.spatial.distance as ssd
+import matplotlib.pyplot as plt
+#import matplotlib.cm as cm
+#import matplotlib.ticker as mt
+#import matplotlib._pylab_helpers as hlp
+#from matplotlib.colors import LogNorm
+#from matplotlib import colors
+#import matplotlib.colors as mplcolors
+#import matplotlib.gridspec as gridspec
+#from functools import partial
+#from collections import OrderedDict
+#import multiprocessing as mp
+#import pandas as pd
+#import statsmodels.api as sm
+#import statsmodels.formula.api as smf
+#from statsmodels.regression.quantile_regression import QuantReg
+#import theil_sen as ts
+#import window_filter as wf
+#import itertools as itt
+
+##--------------------------------------------------------------------------##
+
+## Home-brew robust statistics:
+#try:
+#    import robust_stats
+#    reload(robust_stats)
+#    rs = robust_stats
+#except ImportError:
+#    sys.stderr.write("\nError!  robust_stats module not found!\n"
+#           "Please install and try again ...\n\n")
+#    sys.exit(1)
+
+## Home-brew KDE:
+#try:
+#    import my_kde
+#    reload(my_kde)
+#    mk = my_kde
+#except ImportError:
+#    sys.stderr.write("\nError!  my_kde module not found!\n"
+#           "Please install and try again ...\n\n")
+#    sys.exit(1)
+
+## FITS I/O:
+try:
+    import astropy.io.fits as pf
+except ImportError:
+    try:
+       import pyfits as pf
+    except ImportError:
+        sys.stderr.write("\nError!  No FITS I/O module found!\n"
+               "Install either astropy.io.fits or pyfits and try again!\n\n")
+        sys.exit(1)
+
+##--------------------------------------------------------------------------##
+## Colors for fancy terminal output:
+NRED    = '\033[0;31m'   ;  BRED    = '\033[1;31m'
+NGREEN  = '\033[0;32m'   ;  BGREEN  = '\033[1;32m'
+NYELLOW = '\033[0;33m'   ;  BYELLOW = '\033[1;33m'
+NBLUE   = '\033[0;34m'   ;  BBLUE   = '\033[1;34m'
+NMAG    = '\033[0;35m'   ;  BMAG    = '\033[1;35m'
+NCYAN   = '\033[0;36m'   ;  BCYAN   = '\033[1;36m'
+NWHITE  = '\033[0;37m'   ;  BWHITE  = '\033[1;37m'
+ENDC    = '\033[0m'
+
+## Suppress colors in cron jobs:
+if (os.getenv('FUNCDEF') == '--nocolors'):
+    NRED    = ''   ;  BRED    = ''
+    NGREEN  = ''   ;  BGREEN  = ''
+    NYELLOW = ''   ;  BYELLOW = ''
+    NBLUE   = ''   ;  BBLUE   = ''
+    NMAG    = ''   ;  BMAG    = ''
+    NCYAN   = ''   ;  BCYAN   = ''
+    NWHITE  = ''   ;  BWHITE  = ''
+    ENDC    = ''
+
+## Fancy text:
+degree_sign = u'\N{DEGREE SIGN}'
+
+## Dividers:
+halfdiv = '-' * 40
+fulldiv = '-' * 80
+
+##--------------------------------------------------------------------------##
+## Catch interruption cleanly:
+def signal_handler(signum, frame):
+    sys.stderr.write("\nInterrupted!\n\n")
+    sys.exit(1)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+##--------------------------------------------------------------------------##
+## Save FITS image with clobber (astropy / pyfits):
+def qsave(iname, idata, header=None, padkeys=1000, **kwargs):
+    this_func = sys._getframe().f_code.co_name
+    sys.stderr.write("Writing to '%s' ... " % iname)
+    if header:
+        while (len(header) < padkeys):
+            header.append() # pad header
+    if os.path.isfile(iname):
+        os.remove(iname)
+    pf.writeto(iname, idata, header=header, **kwargs)
+    sys.stderr.write("done.\n")
+
+##--------------------------------------------------------------------------##
+
+
+
+##--------------------------------------------------------------------------##
+##------------------         Parse Command Line             ----------------##
+##--------------------------------------------------------------------------##
+
+## Parse arguments and run script:
+#class MyParser(argparse.ArgumentParser):
+#    def error(self, message):
+#        sys.stderr.write('error: %s\n' % message)
+#        self.print_help()
+#        sys.exit(2)
+#
+### Enable raw text AND display of defaults:
+#class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+#                        argparse.RawDescriptionHelpFormatter):
+#    pass
+#
+### Parse the command line:
+#if __name__ == '__main__':
+#
+#    # ------------------------------------------------------------------
+#    descr_txt = """
+#    PUT DESCRIPTION HERE.
+#    
+#    Version: %s
+#    """ % __version__
+#    parser = argparse.ArgumentParser(
+#            prog='PROGRAM_NAME_HERE',
+#            prog=os.path.basename(__file__),
+#            #formatter_class=argparse.RawTextHelpFormatter)
+#            description='PUT DESCRIPTION HERE.')
+#            #description=descr_txt)
+#    parser = MyParser(prog=os.path.basename(__file__), description=descr_txt)
+#                          #formatter_class=argparse.RawTextHelpFormatter)
+#    # ------------------------------------------------------------------
+#    parser.set_defaults(thing1='value1', thing2='value2')
+#    # ------------------------------------------------------------------
+#    parser.add_argument('firstpos', help='first positional argument')
+#    parser.add_argument('-s', '--site',
+#            help='Site to retrieve data for', required=True)
+#    parser.add_argument('-n', '--number_of_days', default=1,
+#            help='Number of days of data to retrieve.')
+#    parser.add_argument('-o', '--output_file', 
+#            default='observations.csv', help='Output filename.')
+#    parser.add_argument('--start', type=str, default=None, 
+#            help="Start time for date range query.")
+#    parser.add_argument('--end', type=str, default=None,
+#            help="End time for date range query.")
+#    parser.add_argument('-d', '--dayshift', required=False, default=0,
+#            help='Switch between days (1=tom, 0=today, -1=yest', type=int)
+#    parser.add_argument('-e', '--encl', nargs=1, required=False,
+#            help='Encl to make URL for', choices=all_encls, default=all_encls)
+#    parser.add_argument('-s', '--site', nargs=1, required=False,
+#            help='Site to make URL for', choices=all_sites, default=all_sites)
+#    parser.add_argument('-q', '--quiet', action='count', default=0,
+#            help='less progress/status reporting')
+#    parser.add_argument('-v', '--verbose', action='count', default=0,
+#            help='more progress/status reporting')
+#    parser.add_argument('--debug', dest='debug', default=False,
+#            help='Enable extra debugging messages', action='store_true')
+#    parser.add_argument('remainder', help='other stuff', nargs='*')
+#    # ------------------------------------------------------------------
+#    # ------------------------------------------------------------------
+#    ofgroup = parser.add_argument_group('Output format')
+#    fmtparse = ofgroup.add_mutually_exclusive_group()
+#    fmtparse.add_argument('--python', required=False, dest='output_mode',
+#            help='Return Python dictionary with results [default]',
+#            default='pydict', action='store_const', const='pydict')
+#    bash_var = 'ARRAY_NAME'
+#    bash_msg = 'output Bash code snippet (use with eval) to declare '
+#    bash_msg += 'an associative array %s containing results' % bash_var
+#    fmtparse.add_argument('--bash', required=False, default=None,
+#            help=bash_msg, dest='bash_array', metavar=bash_var)
+#    fmtparse.set_defaults(output_mode='pydict')
+#    # ------------------------------------------------------------------
+#    # Miscellany:
+#    miscgroup = parser.add_argument_group('Miscellany')
+#    miscgroup.add_argument('-q', '--quiet', action='count', default=0,
+#            help='less progress/status reporting')
+#    miscgroup.add_argument('-v', '--verbose', action='count', default=0,
+#            help='more progress/status reporting')
+#    miscgroup.add_argument('--debug', dest='debug', default=False,
+#            help='Enable extra debugging messages', action='store_true')
+#    # ------------------------------------------------------------------
+#
+#    context = parser.parse_args()
+#    context.vlevel = 99 if context.debug else (context.verbose-context.quiet)
+#
+##--------------------------------------------------------------------------##
+
+##--------------------------------------------------------------------------##
+## Relevant images:
+lsc_cf_impath = 'geomean_tung01_tung12.fits'
+tung01_images = {
+        'nres01':'images/med_nres01_tung01_20181215_07d_06b_00a.fits.fz',
+        'nres02':'images/med_nres02_tung01_20181215_07d_06b_00a.fits.fz',
+        'nres03':'images/med_nres03_tung01_20181215_07d_06b_00a.fits.fz',
+        }
+
+## Load LSC cal-fiber image:
+sys.stderr.write("Loading %s ... " % lsc_cf_impath)
+cfdata = pf.getdata(lsc_cf_impath)
+sys.stderr.write("done.\n")
+
+## Load images:
+idata, ihdrs = {}, {}
+profile = {}
+want_cols = slice(2048-100, 2048+100, 1)
+for nn,ipath in tung01_images.items():
+    sys.stderr.write("Loading %s ... " % ipath)
+    idata[nn], ihdrs[nn] = pf.getdata(ipath, header=True)
+    ptemp = np.average(idata[nn][:, want_cols], axis=1)
+    ptemp /= np.sum(ptemp)
+    profile[nn] = ptemp
+    sys.stderr.write("done.\n")
+
+##--------------------------------------------------------------------------##
+##--------------------------------------------------------------------------##
+##--------------------------------------------------------------------------##
+
+
+##--------------------------------------------------------------------------##
+fig_dims = (12, 10)
+fig = plt.figure(1, figsize=fig_dims)
+plt.gcf().clf()
+#fig, axs = plt.subplots(2, 2, sharex=True, figsize=fig_dims, num=1)
+# sharex='col' | sharex='row'
+#fig.frameon = False # disable figure frame drawing
+#fig.subplots_adjust(left=0.07, right=0.95)
+#ax1 = plt.subplot(gs[0, 0])
+ax1 = fig.add_subplot(111)
+#ax1 = fig.add_axes([0, 0, 1, 1])
+#ax1.patch.set_facecolor((0.8, 0.8, 0.8))
+#ax1.grid(True)
+#ax1.axis('off')
+
+for kk,prof in profile.items():
+    ax1.plot(prof, label=kk)
+ax1.legend(loc='best')
+
+fig.tight_layout() # adjust boundaries sensibly, matplotlib v1.1+
+plt.draw()
+#fig.savefig(plot_name, bbox_inches='tight')
+
+
+
+######################################################################
+# CHANGELOG (channel_correl_test.py):
+#---------------------------------------------------------------------
+#
+#  2018-12-28:
+#     -- Increased __version__ to 0.0.1.
+#     -- First created channel_correl_test.py.
+#

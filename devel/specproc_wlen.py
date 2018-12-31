8868f91db46403b708ea148074c30941137d2f6f
Rob Siverd <robert.siverd@gmail.com>
2018-12-31 10:38:28 -0600

    Now save summary CSV file to disk for external use.

--- devel/specproc_wlen.py
+++ devel/specproc_wlen.py
@@ -4,6 +4,7 @@ import astropy.io.fits as pf
 
 ## A ~solved LSC spectrum as example:
 specfile = 'lscnrs01-fa09-20181125-0031-e91.fits.fz'
+save_txt = 'stats_specproc_wlen.csv'
 if os.path.isfile(specfile):
     wavespec = pf.getdata(specfile, extname='WAVESPEC')
     wavethar = pf.getdata(specfile, extname='WAVETHAR')
@@ -13,13 +14,24 @@ else:
     sys.exit(1)
 
 def mmm(a):
-    return np.min(a), np.average(a), np.max(a)
+    return (np.min(a), np.average(a), np.max(a))
 
 ## Typical wavelength step in each order:
-for iord,ordwl in list(enumerate(wavethar))[::-1]:
+results = []
+for iord,ordwl in enumerate(wavethar):
     msgtxt  = "iord %3d:\n" % iord
     wstep   = np.diff(ordwl)
     msgtxt += "--> wlen min/avg/max: %10.5f, %10.5f, %10.5f\n" % mmm(ordwl)
     msgtxt += "--> dlam min/avg/max: %10.5f, %10.5f, %10.5f\n" % mmm(wstep)
     sys.stderr.write("%s\n" % msgtxt)
+    results.append([iord] + list(mmm(ordwl)) + list(mmm(wstep)))
+
+## Save a copy to file:
+with open(save_txt, 'w') as f:
+    f.write("order,wl_min,wl_avg,wl_max,dl_min,dl_avg,dl_max\n")
+    for thing in results:
+        f.write(','.join([str(x) for x in thing]) + '\n')
+
+
+sys.exit(0)
 

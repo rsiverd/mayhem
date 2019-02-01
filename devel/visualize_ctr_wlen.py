f3c31eda4429cd8db41dfe84457c8a043665df9f
Rob Siverd <robert.siverd@gmail.com>
2019-02-01 02:18:43 -0600

    Added ccd2spec_xy routine for coordinate transformation (testing)

--- devel/visualize_ctr_wlen.py
+++ devel/visualize_ctr_wlen.py
@@ -2,6 +2,10 @@ import os, sys, time
 import numpy as np
 import astropy.io.fits as pf
 import matplotlib.pyplot as plt
+import theil_sen as ts
+import fov_rotation
+reload(fov_rotation)
+r3d = fov_rotation.Rotate3D()
 
 ## A ~solved LSC spectrum as example:
 specfile = 'lscnrs01-fa09-20181125-0031-e91.fits.fz'
@@ -33,40 +37,28 @@ wavekind = wavethar
 owlmin = np.min(wavekind, axis=1)
 owlmax = np.max(wavekind, axis=1)
 
-#def central_wavelength_xy(lam_cen_nm, ord_sep_pix, wave_sol_nm):
-#    ctr_coo = []
-#    for oi,wlc in enumerate(use_lamcen):
-#        if (wlc < wavekind.min()) or (wlc > wavekind.max()):
-#            continue
-#        for tim_oid,ordwl in enumerate(wavekind):
-#            if (ordwl.min() < wlc) and (wlc < ordwl.max()):
-#                sys.stderr.write("tim_oid: %d\n" % tim_oid) 
-#                wlc_x = np.interp(wlc, ordwl, xpixel)
-#                wlc_y = use_ordsep[tim_oid+1]
-#                ctr_coo.append((wlc, wlc_x, wlc_y))
-
-ctr_coo = []
-centerx = 2048.5
-tim_idx = np.arange(wavekind.shape[0])
-for oi,wlc in enumerate(use_lamcen):
-    which = (owlmin <= wlc) & (wlc <= owlmax)
-    nhits = np.sum(which)
-    sys.stderr.write("nhits = %d for oi=%2d\n" % (nhits, oi))
-    if nhits == 0:
-        continue
-    #possibles = [(ti, np.interp(wlc, ordwl, xpixel)) for ti,ordwl in \
-    #        zip(tim_idx[which], wavekind[which])]
-    xpixels = [np.interp(wlc, ordwl, xpixel) for ordwl in wavekind[which]]
-    #tmp_ti, tmp_xx = zip(*possibles)
-    idx_mid = np.argmin(np.abs(np.array(xpixels) - centerx))
-    ctr_coo.append((oi, xpixels[idx_mid], use_ordsep[oi]))
+#ctr_coo = []
+#centerx = 2048.5
+#tim_idx = np.arange(wavekind.shape[0])
+#for oi,wlc in enumerate(use_lamcen):
+#    which = (owlmin <= wlc) & (wlc <= owlmax)
+#    nhits = np.sum(which)
+#    sys.stderr.write("nhits = %d for oi=%2d\n" % (nhits, oi))
+#    if nhits == 0:
+#        continue
+#    #possibles = [(ti, np.interp(wlc, ordwl, xpixel)) for ti,ordwl in \
+#    #        zip(tim_idx[which], wavekind[which])]
+#    xpixels = [np.interp(wlc, ordwl, xpixel) for ordwl in wavekind[which]]
+#    #tmp_ti, tmp_xx = zip(*possibles)
+#    idx_mid = np.argmin(np.abs(np.array(xpixels) - centerx))
+#    ctr_coo.append((oi, xpixels[idx_mid], use_ordsep[oi]))
     
 def central_wavelength_xy(lam_cen_nm, ord_sep_pix, wave_sol_nm,
-        midpct=(25, 75), xleft=1000, xright=3000):
+        midpct=(25, 75)):
 
     midpoints = []
     wls1, wls2 = np.percentile(wave_sol_nm, midpct, axis=1)
-    for oi,wlc in enumerate(use_lamcen):
+    for oi,wlc in enumerate(lam_cen_nm):
         which = (wls1 <= wlc) & (wlc <= wls2)
         nhits = np.sum(which)
         if nhits == 0:
@@ -80,17 +72,64 @@ def central_wavelength_xy(lam_cen_nm, ord_sep_pix, wave_sol_nm,
         midpoints.append((wlc, thisx, thisy))
     return np.array(midpoints)
 
-    #break
-    #if nhits > 1:
-    #    print(possibles)
-    #    break
-    #ctr_y = use_ordsep[oi]
-    #break
+#ctr_coo = np.array(ctr_coo)
 
-sys.exit(0)
-ctr_coo = np.array(ctr_coo)
-_, cwl_xpix, cwl_ypix = ctr_coo.T
+wl_midpoints_dumb = central_wavelength_xy(1e3 * ctrwl_data['lam_dumb'],
+        ctrwl_data['sep_dumb'], wavethar)
 
+wl_midpoints_iter = central_wavelength_xy(1e3 * ctrwl_data['lam_iter'],
+        ctrwl_data['sep_iter'], wavethar)
+
+_, ccdx_dumb, ccdy_dumb = wl_midpoints_dumb.T
+_, ccdx_iter, ccdy_iter = wl_midpoints_iter.T
+
+## -----------------------------------------------------------------------
+## Linear fit to central wavelengths:
+icept, slope = ts.linefit(ccdx_iter, ccdy_iter)
+fitted_x = np.array([0, 4000])
+fitted_y = icept + slope * fitted_x
+
+rotation_deg = 90.0 - np.degrees(np.arctan(slope))
+sys.stderr.write("Tilt of central wavelength ridge: %.3f deg\n" % rotation_deg)
+
+## Attempt to return to native spectrograph coordinates:
+camera_xyz = np.vstack((ccdx_iter, ccdy_iter, np.zeros_like(ccdx_iter)))
+native_xyz = r3d.zrot(np.radians(rotation_deg), camera_xyz) # nice!
+spcx, spcy, _ = native_xyz
+
+## -----------------------------------------------------------------------
+## Transform CCD -> spectrograph coordinates:
+def ccd2spec_xy(ccdx, ccdy, rot_deg):
+    if ccdx.shape != ccdy.shape:
+        raise ValueError("CCD coordinate arrays have mismatched shape\n:"
+                + "%s != %s\n" % (str(ccdx.shape), str(ccdy.shape)))
+    if len(ccdx.shape) != 1:
+        raise ValueError("Expected 1-D input, have shape %s" % str(ccdx.shape))
+    # MEMORY INTENSIVE!!
+    #old_dim = ccdx.shape
+    #ccd_xyz = np.vstack((ccdx.flatten(), 
+    #                     ccdy.flatten(),
+    #                     np.zeros(ccdx.size)))
+    ccd_xyz = np.vstack((ccdx, ccdy, np.zeros(ccdx.size)))
+    sx, sy, _ = r3d.zrot(np.radians(rot_deg), ccd_xyz)
+    return np.array(sx), np.array(sy)
+    #return sx.reshape(old_dim), sy.reshape(old_dim)
+
+ny, nx = 4096, 4096
+x_list = (0.5 + np.arange(nx)) / nx - 0.5            # relative (centered)
+y_list = (0.5 + np.arange(ny)) / ny - 0.5            # relative (centered)
+#xx, yy = np.meshgrid(x_list, y_list)                 # relative (centered)
+xx, yy = np.meshgrid(nx*x_list, ny*y_list)           # absolute (centered)
+
+spec_coo = []
+for tx,ty in zip(xx, yy):
+    spec_coo.append(ccd2spec_xy(tx, ty, rotation_deg))
+sxx, syy = (np.vstack(coo) for coo in zip(*spec_coo))
+del spec_coo
+
+
+## -----------------------------------------------------------------------
+## -----------------------------------------------------------------------
 ## -----------------------------------------------------------------------
 ## Plot something sensible ...
 
@@ -99,11 +138,14 @@ fig.clf()
 ax1 = fig.add_subplot(111, aspect='equal')
 ax1.grid()
 
-ax1.scatter(cwl_xpix, cwl_ypix, s=35, lw=0)
+ax1.plot(fitted_x, fitted_y, lw=1, ls=':', c='b')
+ax1.scatter(ccdx_dumb, ccdy_dumb, s=15, lw=0, c='r', label='dumb')
+ax1.scatter(ccdx_iter, ccdy_iter, s=15, lw=0, c='g', label='iter')
 ax1.set_xlim(0, 4100)
 ax1.set_ylim(0, 4100)
 ax1.set_xlabel("X Pixel")
 ax1.set_ylabel("Y Pixel")
+ax1.legend(loc='best')
 
 fig.tight_layout()
 plt.draw()

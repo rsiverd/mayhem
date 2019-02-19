d7a4d005e499f8f4b90f8ca4709923193c35e180
Rob Siverd <robert.siverd@gmail.com>
2019-02-18 22:19:29 -0600

    Added vacuum <---> air wavelength conversion formulae.

--- modules/wavelength_reference.py
+++ modules/wavelength_reference.py
@@ -97,6 +97,38 @@ except ImportError:
 
 ##--------------------------------------------------------------------------##
 ##--------------------------------------------------------------------------##
+## Refractive index of air calculation (from Bacigalupo thesis):
+def n_air(wlen_um, t=18., p=101325.):
+    nminus1 = 0.0472326 / (173.3 - (1.0 / wlen_um)**2)
+    return 1.0 + nminus1
+
+## IAU vacuum -> air wavelength conversion (Angstroms):
+## https://classic.sdss.org/dr7/products/spectra/vacwavelength.html
+##   AIR = VAC / (1.0 + 2.735182E-4 + 131.4182 / VAC^2 + 2.76249E8 / VAC^4)
+def iau_vac2air(lam_vac_aa):
+    denom = (1.0 + 2.735182e-4 + 131.4182 / lam_vac_aa**2 \
+                                + 2.76249e8 / lam_vac_aa**4)
+    return lam_vac_aa / denom
+
+## Tim's code to convert air --> vacuum wavelength:
+#function airlam,lam,z0
+#; This routine returns lam(vacuum), with input lam(air) in microns,
+#; with pressure such that n(NaD = 0.589 micron)=1.+z0.
+#; For dry air at STP, set z0 = 0.000295
+#; result is returned in a double-precision vector
+#
+#lamm2=1./lam^2
+#nam2=1./0.589d0^2
+#zs=0.05792105d0/(238.0185d0-lamm2) + 0.00167917d0/(57.362d0-lamm2) 
+#zna=0.05792105d0/(238.0185d0-nam2) + 0.00167917d0/(57.362d0-nam2) 
+#
+#zair=zs*z0/zna
+#
+#return,lam*(1.d0+zair)
+
+
+##--------------------------------------------------------------------------##
+##--------------------------------------------------------------------------##
 ## Track down line lists:
 mayhem_root = os.path.join(os.getenv('HOME'), 'NRES', 'mayhem')
 if not os.path.isdir(mayhem_root):
